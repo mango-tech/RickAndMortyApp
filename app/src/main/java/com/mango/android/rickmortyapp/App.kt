09cddf2172cf65ae9edd65730761605ac7d3d67a
jccarrillocodes <carrialpine@gmail.com>
2021-08-07 15:50:39 +0200

    Added Hilt dependencies and enabled Multidex for app

---
+++ app/src/main/java/com/mango/android/rickmortyapp/App.kt
@@ -0,0 +1,8 @@
+package com.mango.android.rickmortyapp
+
+import androidx.multidex.MultiDexApplication
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : MultiDexApplication() {
+}
