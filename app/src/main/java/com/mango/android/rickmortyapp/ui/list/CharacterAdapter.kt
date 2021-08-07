a1c21c9e83668d376f79e2930b85303e729d49ae
jccarrillocodes <carrialpine@gmail.com>
2021-08-07 22:22:57 +0200

    Added Picasso and bound character image

--- app/src/main/java/com/mango/android/rickmortyapp/ui/list/CharacterAdapter.kt
+++ app/src/main/java/com/mango/android/rickmortyapp/ui/list/CharacterAdapter.kt
@@ -1,11 +1,12 @@
 package com.mango.android.rickmortyapp.ui.list
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.mango.android.domain.entity.CharacterEntity
+import com.mango.android.rickmortyapp.R
 import com.mango.android.rickmortyapp.databinding.ItemCharacterBinding
+import com.squareup.picasso.Picasso
 
 class CharacterAdapter : RecyclerView.Adapter<CharacterAdapter.ViewHolder>() {
 
@@ -19,16 +20,26 @@ class CharacterAdapter : RecyclerView.Adapter<CharacterAdapter.ViewHolder>() {
         fun bind(item: CharacterEntity) {
             binding.root.tag = item
             binding.textViewName.text = item.name
+
+            if (item.image.isNotEmpty())
+                Picasso.get().load(item.image).error(R.drawable.ic_no_image_24)
+                    .into(binding.imageViewAvatar)
         }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(ItemCharacterBinding.inflate(LayoutInflater.from(parent.context)).also {
-            it.root.setOnClickListener { view ->
-                if (view.tag is CharacterEntity)
-                    onItemClick?.invoke(view.tag as CharacterEntity)
-            }
-        })
+        return ViewHolder(
+            ItemCharacterBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            ).also {
+                it.root.setOnClickListener { view ->
+                    if (view.tag is CharacterEntity)
+                        onItemClick?.invoke(view.tag as CharacterEntity)
+                }
+            })
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
