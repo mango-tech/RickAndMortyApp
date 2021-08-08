ef4423be765ca726168d7fd121d56a2cf4c7d069
jccarrillocodes <carrialpine@gmail.com>
2021-08-08 10:56:23 +0200

    Added DetailActivity.kt and removed old java activities and dialog

--- app/src/main/java/com/mango/android/rickmortyapp/ui/NavigatorController.kt
+++ app/src/main/java/com/mango/android/rickmortyapp/ui/NavigatorController.kt
@@ -1,24 +1,39 @@
 package com.mango.android.rickmortyapp.ui
 
 import android.app.Activity
+import android.app.ActivityOptions
 import android.content.Context
 import android.content.Intent
+import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityOptionsCompat
 import com.mango.android.rickmortyapp.ui.alertError.AlertErrorDialog
+import com.mango.android.rickmortyapp.ui.detail.DetailActivity
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
+import androidx.core.util.Pair
+import java.util.*
+import kotlin.collections.ArrayList
 
 class NavigatorController @Inject constructor(@ApplicationContext private val context: Context) {
 
-    fun openDetail(activity: AppCompatActivity, characterId: Int, onResult: () -> Unit) {
-//        var resultLauncher =
-//            activity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-//                onResult.invoke()
-//            }
-//        val intent = Intent(activity, SomeActivity::class.java)
-//        intent.putExtra(DETAIL_EXTRA_CHAR_ID, characterId)
-//        resultLauncher.launch(intent)
+    fun openDetail(
+        activity: AppCompatActivity,
+        viewRelation: Pair<View, String>,
+        characterId: Int,
+        onResult: () -> Unit
+    ) {
+        val resultLauncher =
+            activity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                onResult.invoke()
+            }
+
+        val options =
+            ActivityOptionsCompat.makeSceneTransitionAnimation(activity, viewRelation)
+        val intent = Intent(activity, DetailActivity::class.java)
+        intent.putExtra(DETAIL_EXTRA_CHAR_ID, characterId)
+        resultLauncher.launch(intent, options)
     }
 
     fun openError(activity: AppCompatActivity, onResult: () -> Unit) {
