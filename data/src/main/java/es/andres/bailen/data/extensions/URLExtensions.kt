aefbbc1d6b597859f2efd3087f051d42afa96a5a
Andres Bailen Jimenez <andresbailen93@gmail.com>
2022-06-13 18:42:52 +0200

    [TEST] - init testing

--- data/src/main/java/es/andres/bailen/data/extensions/URLExtensions.kt
+++ data/src/main/java/es/andres/bailen/data/extensions/URLExtensions.kt
@@ -3,7 +3,7 @@ package es.andres.bailen.data.extensions
 import android.net.Uri
 import java.net.URL
 
-fun String.getQueryParameter(paramName: String): Int? {
+fun String.getIntQueryParameter(paramName: String): Int? {
     return try {
         Uri.parse(this).getQueryParameter(paramName)?.toInt()
     }catch (e: Exception) {
--- data/src/main/java/es/andres/bailen/data/repository/datasource/CharacterListDataSource.kt
+++ data/src/main/java/es/andres/bailen/data/repository/datasource/CharacterListDataSource.kt
@@ -2,7 +2,7 @@ package es.andres.bailen.data.repository.datasource
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
-import es.andres.bailen.data.extensions.getQueryParameter
+import es.andres.bailen.data.extensions.getIntQueryParameter
 import es.andres.bailen.data.mappers.mapCharacterDataToModel
 import es.andres.bailen.data.network.api.RickyMortyApi
 import es.andres.bailen.domain.models.CharacterModel
@@ -21,8 +21,8 @@ class CharacterListDataSource(private val api: RickyMortyApi): PagingSource<Int,
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, CharacterModel> {
         return withContext(Dispatchers.IO) {
             val pagingModel  = api.getCharactersList(params.key ?: 0)
-            val prevKey: Int? = pagingModel.info.prev?.getQueryParameter("page")
-            val nextKey: Int? = pagingModel.info.next?.getQueryParameter("page")
+            val prevKey: Int? = pagingModel.info.prev?.getIntQueryParameter("page")
+            val nextKey: Int? = pagingModel.info.next?.getIntQueryParameter("page")
             LoadResult.Page(
                 pagingModel.results.map { mapCharacterDataToModel(it) },
                 prevKey,
